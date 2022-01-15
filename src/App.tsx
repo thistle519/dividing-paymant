8524b3a4d01236b76025f446d144e290269c2e12
thistle519 <shizuru519@gmail.com>
2022-01-15 18:25:05 +0900

    add:人を追加できるようになった

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,66 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [prices, setPrices] = useState<number[]>([]);
+  type Payer = "me" | "both" | "else";
+  type PaymentRecord = { payer: Payer; price: number };
+  const [paymentRecords, setPaymentRecords] = useState<PaymentRecord[]>([]);
   const [inputPrice, setInputPrice] = useState(0);
+  const [inputPayer, setInputPayer] = useState<Payer>("both");
   return (
     <>
-      <input
-        onChange={(e) => {
-          setInputPrice(parseInt(e.target.value));
+      <form
+        onSubmit={(e) => {
+          setPaymentRecords([
+            ...paymentRecords,
+            { payer: inputPayer, price: inputPrice },
+          ]);
+          e.preventDefault();
         }}
-      ></input>
-      <button
-        onClick={() => {
-          setPrices([...prices,inputPrice]);
-        }}
-      ></button>
-      <p>{prices}</p>
+      >
+        {/* {JSON.stringify(paymentRecords, null, 2)} */}
+        <br />
+        <label htmlFor="me">わたし</label>
+        <input
+          type="radio"
+          name="payer"
+          value="me"
+          id="me"
+          onChange={(e) => setInputPayer(e.target.value as "me")}
+        />
+        <label htmlFor="both">ふたり</label>
+        <input
+          type="radio"
+          name="payer"
+          value="both"
+          id="both"
+          onChange={(e) => setInputPayer(e.target.value as "both")}
+        />
+        <label htmlFor="else">あいて</label>
+        <input
+          type="radio"
+          name="payer"
+          value="else"
+          id="else"
+          onChange={(e) => setInputPayer(e.target.value as "else")}
+        />
+        <input
+          type="number"
+          onChange={(e) => {
+            setInputPrice(parseInt(e.target.value));
+          }}
+        />
+        <button
+        // onClick={() => {
+        //   setPrices([...prices, inputPrice]);
+        // }}
+        >
+          追加
+        </button>
+      </form>
+      {paymentRecords.map((pr) => (
+        <li>{pr.price}円{pr.payer}</li>
+      ))}
     </>
   );
 }
