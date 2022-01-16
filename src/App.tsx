119d0b152ec6d95bf237f96eb7d1e6004398583d
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2022-01-16 10:52:03 +0900

    上部のデザインを修正

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,29 @@ export type Payer = "me" | "both" | "else";
 export type PaymentRecord = { payer: Payer; price: number };
 function App() {
   const [paymentRecords, setPaymentRecords] = useState<PaymentRecord[]>([]);
-  const myTotalAmount = paymentRecords.length !== 0 ?
-    paymentRecords.map((pr) => {
-      if (pr.payer === "me") {
-        return pr.price;
-      }
-      if (pr.payer === "both") {
-        return pr.price / 2;
-      }
-      return 0;
-    })
-    .reduce((prev, current) => prev + current) : 0
+  const myTotalAmount =
+    paymentRecords.length !== 0
+      ? paymentRecords
+          .map((pr) => {
+            if (pr.payer === "me") {
+              return pr.price;
+            }
+            if (pr.payer === "both") {
+              return pr.price / 2;
+            }
+            return 0;
+          })
+          .reduce((prev, current) => prev + current)
+      : 0;
 
   return (
     <div className="flex justify-center px-4">
       <div className="w-screen">
         <div className="fixed top-0 h-14 bg-white">
-          <p className="my-4 ">自分が払うべきお金は{myTotalAmount}円です</p>
+          <p className="my-4 ">
+            自分が払うべきお金は{" "}
+            <span className="font-bold text-lg">{myTotalAmount}</span> 円です
+          </p>
         </div>
         <div className="h-full mt-14 mb-52">
           <RecordsList
